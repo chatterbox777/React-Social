db6cf1fc03af4d502492695d9cc91fea5725b634
Vasya <chatterbox1995@mail.ru>
2019-10-26 11:03:57 +0300

    7
    state -> _state in store , make redux concept

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,76 @@
 import { statement } from "@babel/template";
-import { rerenderEntireTree } from "../render";
 
-  let state = {
+  let store = {
+    _state: {
 
-    profilePage: {
-        posts: [
-            {id: 1, message: "Hello, whats happend to you last night?", likeCount: 11}, 
-            {id: 2, message: "Sorry, its not adress to you", likeCount: 76},
-            {id:3, message: 'omg', likeCount: 120}
-          ],
-
-        newPostText: 'chatterbox777'
-    
-        
+      profilePage: {
+          posts: [
+              {id: 1, message: "Hello, whats happend to you last night?", likeCount: 11}, 
+              {id: 2, message: "Sorry, its not adress to you", likeCount: 76},
+              {id:3, message: 'omg', likeCount: 120}
+            ],
+  
+          newPostText: 'chatterbox777'
+      
+          
+      },
+  
+      
+      dialogsPage: {
+  
+          dialogs: [
+              {id: 1, name: 'Artyom', url: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'}, 
+              {id: 2, name: 'Vasiliy', url: 'https://photostocky.ru/wp-content/uploads/2019/02/krutye-foto-i-kartinki-v-vk-1.jpg'},
+              {id: 3, name: 'Dmitriy', url: 'https://lizaalert.org/forum/download/file.php?avatar=10822_1440823850.jpg'},
+              {id: 4, name: 'Nik', url: 'https://klike.net/uploads/posts/2019-03/1551511804_3.jpg'}
+            ],
+  
+          messages: [
+              {id: 1, message: 'Hey'}, 
+              {id: 2, message: 'WazzUPPP'},
+              {id: 3, message: 'nothing interesting , AND YOU?'},
+              {id: 4, message: 'IM TOOOOO'}
+            ]
+      },
+      
+      sitebarPage: {
+  
+      }
+    },
+    _callSubscriber() {
+      console.log ('state was changed');
     },
 
-    
-    dialogsPage: {
-
-        dialogs: [
-            {id: 1, name: 'Artyom', url: 'https://klike.net/uploads/posts/2019-03/1551511801_1.jpg'}, 
-            {id: 2, name: 'Vasiliy', url: 'https://photostocky.ru/wp-content/uploads/2019/02/krutye-foto-i-kartinki-v-vk-1.jpg'},
-            {id: 3, name: 'Dmitriy', url: 'https://lizaalert.org/forum/download/file.php?avatar=10822_1440823850.jpg'},
-            {id: 4, name: 'Nik', url: 'https://klike.net/uploads/posts/2019-03/1551511804_3.jpg'}
-          ],
-
-        messages: [
-            {id: 1, message: 'Hey'}, 
-            {id: 2, message: 'WazzUPPP'},
-            {id: 3, message: 'nothing interesting , AND YOU?'},
-            {id: 4, message: 'IM TOOOOO'}
-          ]
+    getState () {
+      debugger;
+      return this._state;
     },
-    
-    sitebarPage: {
 
-    }
+    addPost() {
+      debugger;
+      let newPost = {
+        id:5,
+        message: this._state.profilePage.newPostText,
+        likeCount: 0
+      };
+  
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+  },
+
+  updateNewPostText (newText) {
+
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe (observer) {
+    this._callSubscriber = observer;
+}
   }
 
-  window.state = state;
 
-export let addPost = () => {
-    debugger;
-    let newPost = {
-      id:5,
-      message: state.profilePage.newPostText,
-      likeCount: 0
-    };
+window.store = store;
 
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-
-export let updateNewPostText = (newText) => {
-
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-  export default state;
+  export default store;
