8e12e75981398ba29fbfca58250bca7e9deed5d7
Vasya <chatterbox1995@mail.ru>
2020-01-14 21:11:24 +0300

    19
    React JS - cookie, login, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import classTags from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
     <header className = {classTags.header}>
     <img src = 'https://placeit-assets.s3-accelerate.amazonaws.com/landing-pages/logo-maker-landing-page-v2/Esport-logo-maker-min.png'></img>
+    <div className = {classTags.loginBlock}>
+      {props.isAuth ? props.login :
+    <NavLink to = '/Login'>Login</NavLink> }
+    </div>
   </header>
     );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducers from './profile-reducer';
 import dialogReducer from './dialogs-reducer';
 import sideBarReducer from './sidebar-reducer';
 import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers ({
     profilePage: profileReducers,
     dialogsPage: dialogReducer,
     sidebarPage: sideBarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore (reducers);
