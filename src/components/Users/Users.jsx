633a7bbc1b87582289098b32ba95d7291e42f8a4
Vasya <chatterbox1995@mail.ru>
2020-01-15 18:55:22 +0300

    20
    follow, unfollow - api + withCredentials: true

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../assets/images/icon_user_default.png';
 import { NavLink } from 'react-router-dom';
-
+import * as axios from 'axios';
 
 
 
@@ -34,8 +34,41 @@ let Users = (props) => {
          </div>
          <div>
              { u.followed 
-             ? <button onClick = {() => {props.unfollow(u.id)}} >Unfollow</button> 
-             : <button onClick = {() => {props.follow(u.id)}} >Follow</button> }
+             ? <button onClick = {() => {
+                
+                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '2fd4ffe9-d26d-45b2-8194-7b74d57a4656'
+                    }
+                })
+                .then(response => {
+                   if (response.data.resultCode === 0) {
+                    props.unfollow(u.id);
+                   }
+
+                });
+                
+            }} >Unfollow</button> 
+            
+            : <button onClick = {() => {
+
+                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '2fd4ffe9-d26d-45b2-8194-7b74d57a4656'
+                    }
+                })
+                .then(response => {
+                   if (response.data.resultCode === 0) {
+                    props.follow(u.id);
+                   }
+                    
+                });
+
+                
+               
+               }} >Follow</button> }
              
          </div>
      </span>
