bd5b3a3c2d1b3c23535a7f41db284bf054c2422a
Vasya <chatterbox1995@mail.ru>
2020-01-19 15:09:35 +0300

    23
    redux-thunk

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,6 +3,7 @@ import styles from './users.module.css';
 import userPhoto from '../../assets/images/icon_user_default.png';
 import { NavLink } from 'react-router-dom';
 import * as axios from 'axios';
+import { usersAPI } from '../../api/api';
 
 
 
@@ -34,45 +35,12 @@ let Users = (props) => {
          </div>
          <div>
              { u.followed 
-             ? <button disabled = {props.followingInProgress.some (id => id === u.id)} onClick = {() => {
-              
-                props.toggleFollowingProgress(true, u.id);
-                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-                    withCredentials: true,
-                    headers: {
-                        'API-KEY': '903da253-799a-43d5-b336-c341bbe7ea4a'
-                    }
-                })
-                .then(response => {
-                   if (response.data.resultCode === 0) {
-                    props.unfollow(u.id);
-                   }
-                   props.toggleFollowingProgress(false, u.id);
-                });
-                
-            }} >Unfollow</button> 
+             ? <button disabled = {props.followingInProgress
+                .some (id => id === u.id)} onClick = {() => { props.unfollow(u.id)}}
+                 >Unfollow</button> 
             
-            : <button disabled = {props.followingInProgress.some (id => id === u.id)} onClick = {() => {
-       
-                props.toggleFollowingProgress(true, u.id);
-
-                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-                    withCredentials: true,
-                    headers: {
-                        'API-KEY': '903da253-799a-43d5-b336-c341bbe7ea4a'
-                    }
-                })
-                .then(response => {
-                   if (response.data.resultCode === 0) {
-                    props.follow(u.id);
-                   }
-                   props.toggleFollowingProgress(false, u.id);
-                    
-                });
-
-                
-               
-               }} >Follow</button> }
+            : <button disabled = {props.followingInProgress.some (id => id === u.id)} onClick = {() => { props.follow(u.id)}}
+             >Follow</button> }
              
          </div>
      </span>
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,10 @@
-import { combineReducers, createStore } from "redux";
+import {applyMiddleware, combineReducers, createStore } from "redux";
 import profileReducers from './profile-reducer';
 import dialogReducer from './dialogs-reducer';
 import sideBarReducer from './sidebar-reducer';
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
+import thunkMiddleware from 'redux-thunk';
 
 let reducers = combineReducers ({
     profilePage: profileReducers,
@@ -13,7 +14,7 @@ let reducers = combineReducers ({
     auth: authReducer
 });
 
-let store = createStore (reducers);
+let store = createStore (reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
